bd3c24d16fe53faad849806150147f353f478df7
Vinicius Jarina <viniciusjarina@gmail.com>
2016-11-24 23:32:09 -0200

    Update TestMaze.cs

--- TestMaze.cs
+++ TestMaze.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using MazeCreator;
 
 using System;
@@ -6,15 +6,38 @@ using System;
 
 namespace MazeCreatorTest
 {
+	public class TestRandomGenerator : IRandomGenerator
+	{
+		Random random = new Random (37);
+
+		public int Next (int max)
+		{
+			return random.Next (max);
+		}
+	}
 	[TestFixture]
 	public class Test
 	{
 		[Test]
 		public void TestDFS ()
 		{
-			var result = Creator.Create (10, 10);
+			var random = new TestRandomGenerator ();
+			var result = Creator.Create (10, 10, Algorithm.DFS, random);
 			string s = result.ToString();
-			Console.WriteLine (s);
+			string expected =
+@"┌───────────┬─┬─┬───┐ 
+│ ┌───┬─╴ ╷ │ │ ╵ ╷ │ 
+├─┘ ╷ ╵ ┌─┤ │ └───┤ │ 
+│ ┌─┴───┘ ╵ ├─┬─╴ │ │ 
+│ └─┐ ╶─┬───┤ │ ╶─┘ │ 
+│ ╷ └─┐ ╵ ╷ ╵ └───┐ │ 
+├─┴─╴ │ ┌─┴───┬───┘ │ 
+│ ╶───┤ │ ╶─┐ │ ╶───┤ 
+│ ┌─╴ ├─┴─┐ │ └───┐ │ 
+│ │ ╶─┘ ╷ ╵ └─┐ ╶─┘ │ 
+└─┴─────┴─────┴─────┘ 
+";
+			Assert.AreEqual (expected, s);
 		}
 	}
 }
