89250b4ed867d201d9dd2d8007f7ba8635a83c26
Vinicius Jarina <viniciusjarina@gmail.com>
2017-09-07 23:52:41 -0300

    Added `CreateAsync` extension method.

---
+++ src/Extensions/ICreatorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using MazeCreator.Core;
+
+namespace MazeCreator.Extensions
+{
+	public static class ICreatorExtensions
+	{
+		public static Task<Maze> CreateAsync (this ICreator creator, int lines, int columns)
+		{
+			return Task.Run (() => creator.Create (lines, columns));
+		}
+	}
+}
